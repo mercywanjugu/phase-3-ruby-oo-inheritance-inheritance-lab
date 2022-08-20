dd03f928d67d1d4a786445f892b88ef5b090f3d8
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-08-20 22:02:04 +0300

    commit tree bc85628d24343485211b5892c427b34d2763a8e1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize(knowledge=[])
+        @knowledge = knowledge
+    end
+
+    def learn(string_arg)
+        @knowledge << string_arg
+    end
 end
